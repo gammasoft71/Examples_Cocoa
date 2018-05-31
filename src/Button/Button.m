6ab22089049f311d5b0150721d35a265116d6747
gammasoft71 <gammasoft71@gmail.com>
2018-05-31 23:46:29 +0200

    Button example review

--- src/Button/Button.m
+++ src/Button/Button.m
@@ -1,46 +1,77 @@
 #include <Cocoa/Cocoa.h>
 
-@interface Button : NSButton
-- (IBAction) OnClick:(id)sender;
-@end
-
-@implementation Button
-- (IBAction) OnClick:(id)sender {
-  NSAlert* alert = [[NSAlert alloc] init];
-  [alert setMessageText:@"Button clicked"];
-  [alert runModal];
-  NSModalSession session = [NSApp beginModalSessionForWindow:[NSApp mainWindow]];
-  [NSApp runModalSession:session];
-  [NSApp endModalSession:session];
+@interface Form : NSWindow {
+  NSButton* button1;
+  NSButton* button2;
+  NSTextField* label1;
+  NSTextField* label2;
+  int button1Clicked;
+  int button2Clicked;
 }
-@end
 
-@interface Form : NSWindow
+- (instancetype) init;
 - (BOOL)windowShouldClose:(id)sender;
+- (IBAction) OnButton1Click:(id)sender;
+- (IBAction) OnButton2Click:(id)sender;
 @end
 
 @implementation Form
+- (instancetype) init {
+  button1Clicked = 0;
+  button2Clicked = 0;
+
+  button1 = [[[NSButton alloc] initWithFrame:NSMakeRect(50, 220, 90, 32)] autorelease];
+  [button1 setTitle:@"button1"];
+  [button1 setBezelStyle:NSBezelStyleRounded];
+  [button1 setTarget:self];
+  [button1 setAction:@selector(OnButton1Click:)];
+  [button1 setAutoresizingMask:NSViewMaxXMargin | NSViewMinYMargin];
+
+  button2 = [[[NSButton alloc] initWithFrame:NSMakeRect(50, 125, 200, 75)] autorelease];
+  [button2 setTitle:@"button2"];
+  [button2 setBezelStyle:NSSmallSquareBezelStyle];
+  [button2 setTarget:self];
+  [button2 setAction:@selector(OnButton2Click:)];
+  [button2 setAutoresizingMask:NSViewMaxXMargin | NSViewMinYMargin];
+  
+  label1 = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 80, 150, 20)];
+  [label1 setStringValue:@"button1 clicked 0 times"];
+  [label1 setBezeled:NO];
+  [label1 setDrawsBackground:NO];
+  [label1 setEditable:NO];
+
+  label2 = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 50, 150, 20)];
+  [label2 setStringValue:@"button2 clicked 0 times"];
+  [label2 setBezeled:NO];
+  [label2 setDrawsBackground:NO];
+  [label2 setEditable:NO];
+
+  [super initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO];
+  [self setTitle:@"Button example"];
+  [[self contentView] addSubview:button1];
+  [[self contentView] addSubview:button2];
+  [[self contentView] addSubview:label1];
+  [[self contentView] addSubview:label2];
+  [self setIsVisible:YES];
+
+  return self;
+}
+
 - (BOOL)windowShouldClose:(id)sender {
   [NSApp terminate:sender];
   return NO;
 }
+
+- (IBAction) OnButton1Click:(id)sender {
+  [label1 setStringValue:[NSString stringWithFormat:@"button1 clicked %d times", ++button1Clicked]];
+}
+
+- (IBAction) OnButton2Click:(id)sender {
+  [label2 setStringValue:[NSString stringWithFormat:@"button1 clicked %d times", ++button2Clicked]];
+}
 @end
 
 int main(int argc, char* argv[]) {
-  // Create Button
-  Button* button1 = [[[Button alloc] initWithFrame:NSMakeRect(10, 265, 90, 32)] autorelease];
-  [button1 setTitle:@"button1"];
-  [button1 setBezelStyle:NSBezelStyleRounded];
-  [button1 setTarget:button1];
-  [button1 setAction:@selector(OnClick:)];
-  [button1 setAutoresizingMask:NSViewMaxXMargin | NSViewMinYMargin];
-
-  // Create Form
-  Form* form1 = [[[Form alloc] initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO] autorelease];
-  [form1 setTitle:@"Button example"];
-  [[form1 contentView] addSubview:button1];
-  [form1 setIsVisible:YES];
-  
   // Gets process name
   NSString* processName = [[NSProcessInfo processInfo] processName];
   
@@ -90,7 +121,8 @@ int main(int argc, char* argv[]) {
   [NSApp setWindowsMenu:windowMenu];
   [NSApp setHelpMenu:helpMenu];
 
-  // Set window as mainWindow
+  // Create Form and set window as mainWindow
+  Form* form1 = [[[Form alloc] init] autorelease];
   [form1 makeMainWindow];
   
   // Creates your own message loop

a8be7f7e586cdb1cd0541833b145e98c65d67e8d
gammasoft71 <gammasoft71@gmail.com>
2018-06-01 14:50:47 +0200

    Label example review

--- src/Label/Label.m
+++ src/Label/Label.m
@@ -1,16 +1,67 @@
 #include <Cocoa/Cocoa.h>
 
-@interface Label : NSTextField
-@end
-
-@implementation Label
-@end
-
-@interface Form : NSWindow
+@interface Form : NSWindow {
+  NSTextField* label1;
+}
+- (instancetype)init;
 - (BOOL)windowShouldClose:(id)sender;
 @end
 
 @implementation Form
+- (instancetype)init {
+  NSMenu* applicationMenu = [[[NSMenu alloc] init] autorelease];
+  [applicationMenu addItem:[[[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"About %@"] , nil), [[NSProcessInfo processInfo] processName]] action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""] autorelease]];
+  [applicationMenu addItem:[NSMenuItem separatorItem]];
+  NSMenuItem* servicesMenuItem = [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Services"], nil) action:nil keyEquivalent:@""];
+  NSMenu* servicesMenu = [[[NSMenu alloc] initWithTitle:@""] autorelease];
+  [applicationMenu setSubmenu:servicesMenu forItem:servicesMenuItem];
+  [applicationMenu addItem:[NSMenuItem separatorItem]];
+  [applicationMenu addItemWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"Hide %@"],nil), [[NSProcessInfo processInfo] processName]] action:@selector(hide:) keyEquivalent:@"h"];
+  NSMenuItem* hideOtherMenuItem = [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Hide Others"] , nil) action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+  [hideOtherMenuItem setKeyEquivalentModifierMask:(NSEventModifierFlagOption|NSEventModifierFlagCommand)];
+  [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Show All"] , nil) action:@selector(unhideAllApplications:) keyEquivalent:@""];
+  [applicationMenu addItem:[NSMenuItem separatorItem]];
+  [applicationMenu addItem:[[[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"Quit %@"] , nil), [[NSProcessInfo processInfo] processName]] action:@selector(terminate:) keyEquivalent:@"q"] autorelease]];
+  NSMenuItem* applicationMenuItem = [[[NSMenuItem alloc] init] autorelease];
+  [applicationMenuItem setSubmenu:applicationMenu];
+  
+  NSMenu* viewMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"View" , nil)] autorelease];
+  NSMenuItem* viewMenuItem = [[[NSMenuItem alloc] init] autorelease];
+  [viewMenuItem setSubmenu:viewMenu];
+  
+  NSMenu* windowMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"Window" , nil)] autorelease];
+  NSMenuItem* windowMenuItem = [[[NSMenuItem alloc] init] autorelease];
+  [windowMenuItem setSubmenu:windowMenu];
+  
+  NSMenu* helpMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"Help" , nil)] autorelease];
+  NSMenuItem* helpMenuItem = [[[NSMenuItem alloc] init] autorelease];
+  [helpMenuItem setSubmenu:helpMenu];
+  
+  NSMenu* mainMenu = [[[NSMenu alloc] init] autorelease];
+  [mainMenu addItem:applicationMenuItem];
+  [mainMenu addItem:viewMenuItem];
+  [mainMenu addItem:windowMenuItem];
+  [mainMenu addItem:helpMenuItem];
+  
+  [NSApp setMainMenu:mainMenu];
+  [NSApp setServicesMenu:servicesMenu];
+  [NSApp setWindowsMenu:windowMenu];
+  [NSApp setHelpMenu:helpMenu];
+  
+  label1 = [[[NSTextField alloc] initWithFrame:NSMakeRect(10, 270, 100, 20)] autorelease];
+  [label1 setStringValue:@"label1"];
+  [label1 setBezeled:NO];
+  [label1 setDrawsBackground:NO];
+  [label1 setEditable:NO];
+  [label1 setSelectable:NO];
+  
+  [super initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO];
+  [self setTitle:@"Label Example"];
+  [[self contentView] addSubview:label1];
+  [self setIsVisible:YES];
+  return self;
+}
+
 - (BOOL)windowShouldClose:(id)sender {
   [NSApp terminate:sender];
   return NO;
@@ -18,88 +69,7 @@
 @end
 
 int main(int argc, char* argv[]) {
-  /// Create Label
-  Label* label1 = [[[Label alloc] initWithFrame:NSMakeRect(10, 270, 200, 20)] autorelease];
-  [label1 setStringValue:@"label1"];
-  [label1 setBezeled:NO];
-  [label1 setDrawsBackground:NO];
-  [label1 setEditable:NO];
-  [label1 setSelectable:NO];
-  
-  // Create Window
-  Form* form1 = [[[Form alloc] initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO] autorelease];
-  [form1 setTitle:@"Label Example"];
-  [[form1 contentView] addSubview:label1];
-  [form1 setIsVisible:YES];
-  
-  // Gets process name
-  NSString* processName = [[NSProcessInfo processInfo] processName];
-  
-  // Creates Application submenu with process name
-  NSMenu* applicationMenu = [[[NSMenu alloc] init] autorelease];
-  [applicationMenu addItem:[[[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"About %@"] , nil), processName] action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""] autorelease]];
-  [applicationMenu addItem:[NSMenuItem separatorItem]];
-  NSMenuItem* servicesMenuItem = [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Services"], nil) action:nil keyEquivalent:@""];
-  NSMenu* servicesMenu = [[[NSMenu alloc] initWithTitle:@""] autorelease];
-  [applicationMenu setSubmenu:servicesMenu forItem:servicesMenuItem];
-  [applicationMenu addItem:[NSMenuItem separatorItem]];
-  [applicationMenu addItemWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"Hide %@"],nil), processName] action:@selector(hide:) keyEquivalent:@"h"];
-  NSMenuItem* hideOtherMenuItem = [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Hide Others"] , nil) action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
-  [hideOtherMenuItem setKeyEquivalentModifierMask:(NSEventModifierFlagOption|NSEventModifierFlagCommand)];
-  [applicationMenu addItemWithTitle:NSLocalizedString([NSString stringWithUTF8String:"Show All"] , nil) action:@selector(unhideAllApplications:) keyEquivalent:@""];
-  [applicationMenu addItem:[NSMenuItem separatorItem]];
-  [applicationMenu addItem:[[[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:NSLocalizedString([NSString stringWithUTF8String:"Quit %@"] , nil), processName] action:@selector(terminate:) keyEquivalent:@"q"] autorelease]];
-  NSMenuItem* applicationMenuItem = [[[NSMenuItem alloc] init] autorelease];
-  [applicationMenuItem setSubmenu:applicationMenu];
-
-  // Creates View submenu
-  NSMenu* viewMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"View" , nil)] autorelease];
-  NSMenuItem* viewMenuItem = [[[NSMenuItem alloc] init] autorelease];
-  [viewMenuItem setSubmenu:viewMenu];
-
-  // Creates Window submenu
-  NSMenu* windowMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"Window" , nil)] autorelease];
-  NSMenuItem* windowMenuItem = [[[NSMenuItem alloc] init] autorelease];
-  [windowMenuItem setSubmenu:windowMenu];
-
-  // Creates Help submenu
-  NSMenu* helpMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"Help" , nil)] autorelease];
-  NSMenuItem* helpMenuItem = [[[NSMenuItem alloc] init] autorelease];
-  [helpMenuItem setSubmenu:helpMenu];
-
-  // Creates main menubar
-  NSMenu* mainMenu = [[[NSMenu alloc] init] autorelease];
-  [mainMenu addItem:applicationMenuItem];
-  [mainMenu addItem:viewMenuItem];
-  [mainMenu addItem:windowMenuItem];
-  [mainMenu addItem:helpMenuItem];
-
-  // Creates Application and asociate menubar and specific menus.
   [NSApplication sharedApplication];
-  [NSApp setMainMenu:mainMenu];
-  [NSApp setServicesMenu:servicesMenu];
-  [NSApp setWindowsMenu:windowMenu];
-  [NSApp setHelpMenu:helpMenu];
-
-  // Set window as mainWindow
-  [form1 makeMainWindow];
-  
-  // Creates your own message loop
-  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-  [NSApp finishLaunching];
-  bool hasIdle = true;
-  while (true) {
-    [pool release];
-    pool = [[NSAutoreleasePool alloc] init];
-    
-    NSEvent* event = [NSApp nextEventMatchingMask:NSEventMaskAny untilDate: hasIdle ? [NSDate distantPast] : [NSDate distantFuture] inMode:NSDefaultRunLoopMode dequeue:YES];
-    if (event != nil) {
-      // run your own dispatcher...
-      [NSApp sendEvent:event];
-      [NSApp updateWindows];
-    } else if (hasIdle) {
-      // run idle method...
-    }
-  }
-  [pool release];
+  [[[[Form alloc] init] autorelease] makeMainWindow];
+  [NSApp run];
 }

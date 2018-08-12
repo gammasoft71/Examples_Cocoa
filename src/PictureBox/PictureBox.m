4a36ad4f6682d65de07a60be785dfdbec6a51b06
Gammasoft71 <gammasoft71@gmail.com>
2018-08-12 23:58:11 +0200

    add PictureBox

---
+++ src/PictureBox/PictureBox.m
@@ -0,0 +1,33 @@
+#include <Cocoa/Cocoa.h>
+
+@interface Form : NSWindow {
+  NSImageView* pictureBox1;
+}
+- (instancetype)init;
+- (BOOL)windowShouldClose:(id)sender;
+@end
+
+@implementation Form
+- (instancetype)init {
+  pictureBox1 = [[[NSImageView alloc] initWithFrame:NSMakeRect(10, 10, 280, 280)] autorelease];
+  [pictureBox1 setImage:[NSImage imageNamed:@"Logo.png"]];
+  [pictureBox1 setImageFrameStyle:(NSImageFrameGrayBezel)];
+
+  [super initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO];
+  [self setTitle:@"PictureBox Example"];
+  [[self contentView] addSubview:pictureBox1];
+  [self setIsVisible:YES];
+  return self;
+}
+
+- (BOOL)windowShouldClose:(id)sender {
+  [NSApp terminate:sender];
+  return YES;
+}
+@end
+
+int main(int argc, char* argv[]) {
+  [NSApplication sharedApplication];
+  [[[[Form alloc] init] autorelease] makeMainWindow];
+  [NSApp run];
+}
---
+++ src/PictureBox/README.md
@@ -0,0 +1,13 @@
+# PictureBox
+
+This example demonstrates the use of NSImageView control.
+
+# Source
+
+[PictureBox.m](./PictureBox.m)
+
+[CMakeLists.txt](./CMakeLists.txt)
+
+# Output
+
+![GitHub Logo](../../docs/Pictures/PictureBox.png)
